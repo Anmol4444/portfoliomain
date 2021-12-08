900a009fe5f344179395c7ce0f2b58a5d97f08db
anmol4444 <anmolmathema@gmail.com>
2021-12-08 23:34:20 +0545

    changes

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing-2", {
+    strings: ["Anmol Mathema", "Web Designer", "Frontend Developer", "Digital Marketer", "Vue Js Developer"],
+    typeSpeed: 70,
+    backSpeed: 20,
+    loop: true
+});
